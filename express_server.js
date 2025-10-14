f2d6d9770cc1b6fe83436b94141d912916cbbfc0
RubnK <40402492+RubnK@users.noreply.github.com>
2025-10-14 11:55:03 +0200

    7. Renvoyer du JSON

--- express_server.js
+++ express_server.js
@@ -10,6 +10,10 @@ app.get('/some-html', (req, res) => {
   res.send('<html><body><h1>bonjour html</h1></body></html>');
 });
 
+app.get('/some-json', (req, res) => {
+  const personne = { age: 22, nom: 'Jane' };
+  res.json(personne);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
